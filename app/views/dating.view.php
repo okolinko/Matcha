e94d7d139fd67c2aca43afbd01e474d3cc094c0b
okolinko <virussania@gmail.com>
2019-06-03 20:21:18 +0300

    form

--- app/views/dating.view.php
+++ app/views/dating.view.php
@@ -4,7 +4,11 @@
 <div class="flex-container">
     <div class="flex-form">1</div>
     <div class="flex-elem">
-        <div ><img class="foto-form"  src="../../public/img/avatar/avatar1.png"></div>
+        <img class="foto-form"  src="../../public/img/avatar/avatar1.png">
+        <div class="name-form">Имя:          Sania</div>
+        <div class="name-form">Пол:          Парень</div>
+        <div class="name-form">Ориентация:   Гетеро</div>
+        <div class="name-form">Возраст:      27</div>
     </div>
     <div class="flex-elem">3</div>
     <div class="flex-elem">4</div>
