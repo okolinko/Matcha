d0997b2fd1359ac77c4d3e7779adc7217f1ee607
rumiko.takahashi.rumiko <rumiko.takahashi.rumiko@gmail.com>
2019-12-24 14:45:51 +0200

    add file

--- core/helpers2.php
+++ core/helpers2.php
@@ -7,3 +7,9 @@ function dd($param)
 	echo '</pre>';
 	exit();
 }
+
+function reportLog($str){
+    $file = ROOT."/var/log/debug.log";
+    file_put_contents($file, "[". date('m/d/Y h:i:s a', time()) . "] ", FILE_APPEND | LOCK_EX);
+    file_put_contents($file, json_encode($str)."\n", FILE_APPEND | LOCK_EX);
+}
