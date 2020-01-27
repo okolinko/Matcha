0289bfa9f193a4ce798d082b3bb6d7e6fb740ece
okolinko <virussania@gmail.com>
2020-01-27 18:38:33 +0200

    t

--- public/js/ban.js
+++ public/js/ban.js
@@ -1,16 +1,16 @@
 $(document).ready(function () {
    if ($("#ban")) {
        addEventListener('click', function (event) {
-           if (event.toElement.src == "https://matcha.loc/public/img/ban.png"){
-               event.toElement.src = "https://matcha.loc/public/img/ban_n.png";
+           if (event.toElement.src == "http://matcha.loc/public/img/ban.png"){
+               event.toElement.src = "http://matcha.loc/public/img/ban_n.png";
               var id_user = (event.toElement.attributes.value.value);
                // alert(id_user);
                $.post("//matcha.loc/userbandell", {
                    idUser: id_user
                    },);
            }
-           else if (event.toElement.src == "https://matcha.loc/public/img/ban_n.png"){
-               event.toElement.src = "https://matcha.loc/public/img/ban.png";
+           else if (event.toElement.src == "http://matcha.loc/public/img/ban_n.png"){
+               event.toElement.src = "http://matcha.loc/public/img/ban.png";
                var id_user = (event.toElement.attributes.value.value);
                // alert(id_user);
                $.post("//matcha.loc/userbanadd", {
