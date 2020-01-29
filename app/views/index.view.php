e536f23a3c2882dec68784315b070ca3d927ef6f
okolinko <virussania@gmail.com>
2020-01-29 14:59:04 +0200

    index

--- app/views/index.view.php
+++ app/views/index.view.php
@@ -1,8 +1,8 @@
 <?php require('partials/head.php'); ?>
 <div class="flex-conteiner">
-	<h1>MATCHA</h1>
+	<center><img src="../../public/img/jiozea5ecr.png" style=" padding-top: 30px; padding-bottom: 30px;"></center>
 </div>
-
+<img style="-webkit-user-select: none;margin: auto;" src="https://site-line.com.ua/images/biznes-na-znakomstvah.jpg" data-insta_upload_ext_elem="1">
 <?php require('partials/footer.php'); ?>
 <script type="text/javascript" src="//matcha.loc/public/js/index.js"></script>
 
